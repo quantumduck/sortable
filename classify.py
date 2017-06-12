1794e22a9bb415b3a1ab0c29d9e4de9fd807a433
quantumduck <quantum.duck@hotmail.com>
2017-06-12 15:34:06 -0400

    beginning match logic

--- classify.py
+++ classify.py
@@ -11,14 +11,15 @@ all_products = [json.loads(line) for line in product_file.readlines()]
 product_file.close()
 
 # Divide by manufacturers first:
-
 manufacturers = [
     {'name': manufacturer.lower(), 'listings': []}
     for manufacturer in set([product['manufacturer']
     for product in all_products])]
 
-manufacturers.append({'name': 'unknown', 'listings': [], 'products': []})
+# Add a catch_all for the listings that don't match:
+manufacturers.append({'name': 'unknown', 'listings': []})
 
+# Populate the manufacturers array with matching listings:
 for listing in all_listings:
     not_found = True
     for manufacturer in manufacturers:
@@ -28,4 +29,28 @@ for listing in all_listings:
     if not_found:
         manufacturers[-1]['listings'].append(listing)
 
-print [m['name'] for m in manufacturers]
+# Set up array for the final results:
+matches = []
+
+# Loop over all the products:
+for product in all_products:
+    matches.append({'product_name': product['product_name'], 'listings': []})
+    # Make a list of the basic info we have
+    key_words = product['model'].lower().split()
+    if 'family' in product:
+        key_words += product['family'].lower().split()
+
+    # Loop over only the listings relevant to that particular manufacturer:
+    manufacturer = filter(
+        lambda man: man['name'] == product['manufacturer'].lower(),
+        manufacturers)[0]
+
+    for listing in manufacturer['listings']:
+        match_count = 0
+        for word in key_words:
+            match_count += listing['title'].lower().split().count('word')
+            print listing['title'].lower().split()
+            print word
+        if match_count > 0:
+            print product['product_name']
+            print listing['title']
