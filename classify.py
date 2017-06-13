1e073dd27b34e8b498f49e3aeeb3881bf586e8c1
quantumduck <quantum.duck@hotmail.com>
2017-06-13 14:33:21 -0400

    refactoring to make methods and adding errors

--- classify.py
+++ classify.py
@@ -1,66 +1,90 @@
 import json
 import re
 
-# Load both the text files:
+# Punctuation stripping method
+def alpha_numeric(string):
+    # This method is a bit crude, but it is only necessary for putting product
+    # names and model numbers into the same format. This code would need to be
+    # modified if any companies have namespaces using non-ASCII characters
+    return re.sub(r'[^a-z0-9]', ' ', string.lower())
 
-listing_file = open('listings.txt', 'r')
-all_listings = [json.loads(line) for line in listing_file.readlines()]
-listing_file.close()
+def find_manufacturer(listing, manufacturers):
+    # Find the manufacturer from the given list
+    manufacturer = False
+    for man in manufacturers:
+        if listing['manufacturer'].lower().find(man) >= 0:
+            if manufacturer:
+                # Do not allow manufacturer to be defined twice:
+                raise ValueError(listing['manufacturer'] + ' matched both ' +
+                                  manufacturer + ' and ' + man)
+            manufacturer = man
+    return manufacturer
 
+def classify(listing, products, manufacturer):
+    # This function returns False or the index of the product that matches
+    match_index = -1
+    for index, product in enumerate(all_products):
+        # Only look at products made by current manufacturer:
+        if product['manufacturer'].lower() == manufacturer:
+            # Create a list of key words:
+            key_words = alpha_numeric(product['model']).split()
+            if 'family' in product:
+                key_words += alpha_numeric(product['family']).split()
+                # Search for key words in title:
+                match = True
+                for word in key_words:
+                    if alpha_numeric(listing['title']).find(word) < 0:
+                        # If a single key word is missing, declare no match:
+                        match = False
+                if match:
+                    if match_index >= 0:
+                        print match_index
+                        print index
+                        raise ValueError(listing['title'] +
+                                    ' matched more than one product:')
+                    match_index = index
+    return match_index
+
+# Load all the products into memory:
 product_file = open('products.txt', 'r')
 all_products = [json.loads(line) for line in product_file.readlines()]
 product_file.close()
 
-def alpha_numeric(string):
-    return re.sub(r'[^a-z0-9]', ' ', string.lower())
-
 # Divide by manufacturers first:
-manufacturers = [
-    {'name': manufacturer.lower(), 'listings': []}
-    for manufacturer in set([product['manufacturer']
-    for product in all_products])]
-# Add a catch_all for the listings that don't match:
-manufacturers.append({'name': 'other', 'listings': []})
-
-# Populate the manufacturers array with matching listings:
-for listing in all_listings:
-    not_found = True
-    for manufacturer in manufacturers:
-        if listing['manufacturer'].lower().find(manufacturer['name']) >= 0:
-            manufacturer['listings'].append(listing)
-            not_found = False
-    if not_found:
-        manufacturers[-1]['listings'].append(listing)
+manufacturers = set([product['manufacturer'].lower()
+                    for product in all_products])
 
 # Set up array for the final results:
-matches = []
+matches = [{'product_name': product['product_name'], 'listings': []}
+            for product in all_products]
 match_count = 0
 
-# Loop over all the products:
-for product in all_products:
-    matches.append({'product_name': product['product_name'], 'listings': []})
-    # Make a list of the basic info we have
-    key_words = alpha_numeric(product['model']).split()
-    if 'family' in product:
-        key_words += alpha_numeric(product['family']).split()
-
-    # Loop over only the listings relevant to that particular manufacturer:
-    manufacturer = filter(
-        lambda man: man['name'] == product['manufacturer'].lower(),
-        manufacturers)[0]
-
-    for listing in manufacturer['listings']:
-        match = True
-        for word in key_words:
-            if alpha_numeric(listing['title']).find(word) < 0:
-                match = False
-        if match:
-            matches[-1]['listings'].append(listing)
+# Go through the listings line by line:
+listing_file = open('listings.txt', 'r')
+current_line = listing_file.readline()
+while current_line != '':
+    # Convert listing to object:
+    listing = json.loads(current_line)
+    # Determine the manufacturer:
+    manufacturer = find_manufacturer(listing, manufacturers)
+    if manufacturer:
+        # Go through all the products to check for a match:
+        product_index = classify(listing, all_products, manufacturer)
+        # If there is a match, put it in the results
+        if product_index >= 0:
+            matches[product_index]['listings'].append(listing)
             match_count += 1
             print match_count
 
-final_answer = open('results.txt', 'w')
+    # Read the next line in the file:
+    current_line = listing_file.readline()
+
+# Close the file when all the listings have been checked:
+listing_file.close()
+
+answer_file = open('results.txt', 'w')
 for product in matches:
+    # Only print out the products that were found:
     if len(product['listings']) > 0:
-        final_answer.write(json.dumps(product) + '\n')
-final_answer.close();
+        answer_file.write(json.dumps(product) + '\n')
+answer_file.close();
